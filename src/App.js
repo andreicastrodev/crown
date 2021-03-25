f6594208dee44756a97433f3a91fd2cce7d1496f
andy dev <krazercastro1@gmail.com>
2021-03-25 12:13:06 +0800

    updated the app with shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-const Hats = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={Hats} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
